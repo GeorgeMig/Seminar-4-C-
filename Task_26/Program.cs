88c32d2609f73da9be1a6028445d95d7b0cdfaf9
George_M <msgoha555@yandex.ru>
2022-05-22 02:32:48 +0300

    Complete Task 26..

--- Task_26/Program.cs
+++ Task_26/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите любое натуральное число => ");
 int number = int.Parse(Console.ReadLine());
 int i = 0;
-int delnum = number; //  переменная для деления
+int delnum = number; //  переменная для деления исходного числа delete number (удаление числа)
 while (delnum > 0)
 {
     delnum = delnum / 10;
