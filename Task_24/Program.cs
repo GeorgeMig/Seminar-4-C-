4417324fffea2a490b6ffa7c36a3349ae201a362
George_M <msgoha555@yandex.ru>
2022-05-22 02:07:18 +0300

    completed Task 24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите любое натуральное число => ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i;
+}
+ Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
