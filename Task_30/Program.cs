667b94a3f29fd0fa3663ba21dfffb36a94859c6a
George_M <msgoha555@yandex.ru>
2022-05-22 03:42:43 +0300

    Complete Task 30

---
+++ Task_30/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Задайте размер масива любым числом => ");
+int numleng = int.Parse(Console.ReadLine());
+int [] array = new int [numleng];
+int i = 0;
+int count = numleng;
+Console.Write("Массив =>");
+while (i < count)
+{
+    array [i] = new Random().Next(0, 2);
+    Console.Write($" |{array[i]}|");
+    i++;
+}
+
