98639198ad81cea1fad14cb6a3bf9083cefa617b
George_M <msgoha555@yandex.ru>
2022-05-28 13:48:37 +0300

    Completed Homework 29

---
+++ Homework_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая задаёт массив целых чисел и выводит их на экран.
+
+Console.Write("Задайте размер масива любым числом => ");
+int numleng = int.Parse(Console.ReadLine());
+int [] array = new int [numleng];
+int i = 0;
+int count = numleng;
+Console.Write("Массив =>");
+while (i < count)
+{
+    array [i] = new Random().Next(1, 100);
+    Console.Write($" |{array[i]}|");
+    i++;
+}
