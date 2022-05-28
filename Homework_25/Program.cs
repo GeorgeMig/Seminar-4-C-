0514ab2384d06b587be4c0f8404c9cd417d06cdf
George_M <msgoha555@yandex.ru>
2022-05-28 13:10:26 +0300

    Completed Homework 25

---
+++ Homework_25/Program.cs
@@ -0,0 +1,15 @@
+﻿// ПРОГРАММА, которая принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B
+
+Console.Write("Введите любое натуральное число, которое необходимо возвести в степень => ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральую степень, в которую необходимо возвести число => ");
+int degree = int.Parse(Console.ReadLine());
+int count = degree;
+int res = num;
+while (count > 0)
+{
+    res = res * num;
+    count --;
+}
+Console.WriteLine($"Число {num} в степени {degree} равняется {res}.");
