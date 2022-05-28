bd3255b8873f2200d30d93c42529689ff1e38f3a
George_M <msgoha555@yandex.ru>
2022-05-28 13:33:50 +0300

    Completed Homework 27

---
+++ Homework_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+Console.Write("Введите любое натуральное число => ");
+int num = int.Parse(Console.ReadLine());
+int countnum = num;
+int secnum = 0;
+int sum = 0;
+while (countnum > 0)
+{
+    secnum = countnum % 10;
+    sum = sum + secnum;
+    countnum = countnum / 10;
+}
+Console.WriteLine($"Сумма цифр в числе {num} равна {sum}.");
