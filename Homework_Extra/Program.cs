a56cc5f71741ed70354685ddcf238863080efbc2
George_M <msgoha555@yandex.ru>
2022-06-05 18:43:46 +0300

    Complete seminar 4 & extra homework

---
+++ Homework_Extra/Program.cs
@@ -0,0 +1,20 @@
+﻿//метод, который принимает слово и возвращает true, если в этом слове есть две одинаковые, стоящие рядом буквы.
+
+Console.Write("Введите любое слово => ");
+string word = Console.ReadLine();
+string res = Findres(word);
+
+string Findres(string pair)
+{
+    string ress = "false";
+    for( int i = 0; i < word.Length - 1; i++)
+{
+    if (pair [i] == pair [i + 1])
+    {
+         ress = "true";
+    }
+}
+return ress;
+}
+Console.WriteLine(res);
+
